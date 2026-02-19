3b14fa5e6a708c51e44ccafd3120a782b8719e2e
Anton Gorelkin <116415676+antongorelkin@users.noreply.github.com>
2026-02-19 15:12:21 +0000

    build

--- src/app/pages/order/order.ts
+++ src/app/pages/order/order.ts
@@ -3,6 +3,7 @@ import { Header } from '../../header/header';
 import { DELIVERY_SIZES, DELIVERY_SPEEDS } from './order.config';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UpperCasePipe } from '@angular/common';
+import { DeliveryApi } from '../../services/delivery-api';
 declare var ymaps: any;
 
 @Component({
@@ -23,9 +24,10 @@ export class Order {
 
   public orderId: any = signal(null);
   public calculationResult: any = signal(null);
+  public isLoading: any = signal(false);
 
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private deliveryApi: DeliveryApi) {
     this.routeForm = this.formBuilder.group({
       from: ['', Validators.required],
       to: ['', Validators.required],
@@ -75,6 +77,9 @@ export class Order {
       this.mapRoute = null;
     }
 
+    // Показываем лоадер: идут расчеты маршрута
+    this.isLoading.set(true);
+
     this.mapRoute = new ymaps.multiRouter.MultiRoute(
       { referencePoints: [from, to] },
       { boundsAutoApply: false }
@@ -112,6 +117,8 @@ export class Order {
           total,
           speed
         });
+        // Скрываем лоадер после успешного расчета
+        this.isLoading.set(false);
       } catch (err) {
         this.failedCalculation();
       }
@@ -122,6 +129,7 @@ export class Order {
 
   private failedCalculation() {
     this.calculationResult.set(null);
+    this.isLoading.set(false);
     alert('Не удалось построить маршрут. Проверьте адреса и выбранные параметры.');
   }
 
@@ -148,7 +156,14 @@ export class Order {
       createdAt: new Date().toISOString()
     };
 
-    console.log(payload);
+    this.deliveryApi.createDelivery(payload).subscribe((response) => {
+      if ('error' in response) {
+        alert(response.error);
+        return;
+      }
+
+      this.orderId.set(response.id);
+    });
     this.orderId.set(1);
   }
 }
